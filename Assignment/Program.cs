4c6a0fc53d471287110e51b226fcd716a716f120
AbdallahSay <luka3122003@gmail.com>
2025-06-26 21:52:06 +0300

    Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            #region 1-	Write a program that allows the user to enter a number then print it.
+            //Console.WriteLine("Please Enter Number");
+            //bool isParsedNumber = int.TryParse(Console.ReadLine(), out int number);
+            //Console.Clear();
+            //Console.WriteLine($"isParsedNumber : {isParsedNumber}");
+            //Console.WriteLine($"number is : {number}");
+
+            #endregion
+
+          
+        }
+    }
+}
