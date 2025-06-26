1fa9c477cc76d2be4199c1fcf1fcd4d6f60d63e0
AbdallahSay <luka3122003@gmail.com>
2025-06-26 13:39:26 +0300

    ToString

--- Demo2/Program.cs
+++ Demo2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Demo2
+﻿using System.Drawing;
+
+namespace Demo2
 {
     internal class Program
     {
@@ -39,6 +41,26 @@
             //int Number = 20;
             //int Result = Number + (int)obj; //inValid (Remove Type Safety)
             //Console.WriteLine(Result); // Runtime Erorr
+            #endregion
+            #region ToString
+            //object o = null;
+            //string s = o.ToString(); // runtime error (NullReferenceException) 
+            // لازم تتاكد ان object != Null 
+            //int x = 10;
+            //int y = 20;
+            //Console.WriteLine(x.ToString()+y.ToString()); // 1020 Beacuse X , Y Represent as String Not int
+            //Point p1 = new Point() { x = 4, y = 8 }; //  Initializer Object
+            //Point p2 = new Point() { x = 410 , y = 30 };
+
+            //Console.WriteLine(p1.ToString());//Demo2.Point // Default Implementaion (Default Behavior (Object) )
+            //Console.WriteLine(p2.ToString()); // Object (Return Path Default Behavior)
+
+            //Most type override this method
+            //int x = 10;
+            //Console.WriteLine(x.ToString()); // 10 (1/ Call ToSring object , 2/ Call back Tostring Int32 because Behavior struct Types override Tostring dafault behavior)
+            string name;
+            name = "Abdol";
+            Console.WriteLine(name.ToString()); // abdol (1/ Call ToSring object , 2/ Call back Tostring string type because Behavior string Types override Tostring dafault behavior)
             #endregion
             #endregion
 
