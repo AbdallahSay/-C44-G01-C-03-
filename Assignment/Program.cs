05ae16059f88ab2159815ba1c93da46727c62c26
AbdallahSay <luka3122003@gmail.com>
2025-06-26 21:56:50 +0300

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            
+
 
             #region 1-	Write a program that allows the user to enter a number then print it.
             //Console.WriteLine("Please Enter Number");
@@ -14,8 +14,14 @@
             //Console.WriteLine($"number is : {number}");
 
             #endregion
+            #region 2-	Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen 
+            //string name = "Abdol";
+            //int Number = Convert.ToInt32(name);
+            //Console.WriteLine(Number); // throw format exception
+            #endregion
 
-          
+           
+
         }
     }
 }
